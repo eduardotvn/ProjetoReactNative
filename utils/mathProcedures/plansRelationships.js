57f12b0c3532654d2c69c84a375601db609aaf77
Eduardo Tavares <eduardo_tvn@hotmail.com>
2024-02-29 14:34:35 -0300

    Pequena mexida

--- utils/mathProcedures/plansRelationships.js
+++ utils/mathProcedures/plansRelationships.js
@@ -1,5 +1,6 @@
 
 import { GetAllDailySpendData } from "../firebaseRequests/getAllDocs";
+import { getPlanningData } from "../firebaseRequests/planOptionsRequests";
 
 
 export const SumMonthlySpend = async (userUID, category, month, year) => {
@@ -27,3 +28,23 @@ export const SumMonthlySpend = async (userUID, category, month, year) => {
         throw error;
     }
 }
+
+export const FindRelations = async (userUID, month, year) => {
+    const plans = await getPlanningData(userUID)
+
+    const result = await Promise.all(
+        plans.map(async (plan) => {
+            const category = plan.Category;
+            const spent = await SumMonthlySpend(userUID, category, month, year);
+            const remainingGoal = plan.Goal - spent;
+
+            return {
+                id: plan.id,
+                category,
+                spent,
+                remainingGoal
+        }})
+    )
+
+    
+} 
